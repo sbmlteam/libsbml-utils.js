ad9e5666ff171ab505980e6473cd68f817df5561
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2026-04-10 17:42:59 +0200

    - split example for validation / conversion

--- test/test_conversion.js
+++ test/test_conversion.js
@@ -1,18 +1,6 @@
 var utils = require('./sbml_utils.js');
 var fs = require("fs");
 
-var validationOptions = {
-    "LIBSBML_CAT_GENERAL_CONSISTENCY": true,
-    "LIBSBML_CAT_IDENTIFIER_CONSISTENCY": true,
-    "LIBSBML_CAT_UNITS_CONSISTENCY": false,
-    "LIBSBML_CAT_MATHML_CONSISTENCY": true,
-    "LIBSBML_CAT_SBO_CONSISTENCY": true,
-    "LIBSBML_CAT_OVERDETERMINED_MODEL": true,
-    "LIBSBML_CAT_MODELING_PRACTICE": true,
-    "LIBSBML_CAT_STRICT_UNITS_CONSISTENCY": false,
-};
-
-
 utils().then((Module) => {
 
     // instantiate validator
@@ -23,18 +11,6 @@ utils().then((Module) => {
 
     // load a local file using the filesystem 
     var data = fs.readFileSync(process.argv[2], 'utf8');
-
-    // print validation errors
-    console.log(instance.validateSBMLString(data, JSON.stringify(validationOptions)));
-
-    // print available converters
-    var converters = instance.getAvailableConverters();
-    console.log("num available converters: ", converters.size());
-    for (var i = 0; i < converters.size(); i++) {
-        console.log(converters.get(i));
-        console.log(instance.getConverterOptions(converters.get(i)));
-        console.log("--------------------------------");
-    }
 
     // get all options at once
     console.log(instance.getAllConvertersOptions());
@@ -48,6 +24,8 @@ utils().then((Module) => {
         "targetNamespaces":{"level":3,"version":2}
     })));
 
+    console.log(instance.getConversionErrors());
+
     // and l2v4
     console.log(instance.convertSBMLString(data, JSON.stringify({
         "options":[
@@ -56,5 +34,23 @@ utils().then((Module) => {
         ],
         "targetNamespaces":{"level":2,"version":4}
     })));
+
+    console.log(instance.getConversionErrors());
+
+    // a conversion with simplified options
+    console.log(instance.convertSBMLString(data, JSON.stringify({
+        "setLevelAndVersion":"true",
+        "strict":"false",
+        "targetNamespacesx":{"level":1,"version":2}
+    })));
+
+    console.log(instance.getConversionErrors());
     
+
+    // and an invalid conversion
+    console.log(instance.convertSBMLString(data, JSON.stringify(
+        ["something","invalid"]
+    )));
+
+    console.log(instance.getConversionErrors());
 });
---
+++ test/test_validation.js
@@ -0,0 +1,30 @@
+var utils = require('./sbml_utils.js');
+var fs = require("fs");
+
+var validationOptions = {
+    "LIBSBML_CAT_GENERAL_CONSISTENCY": true,
+    "LIBSBML_CAT_IDENTIFIER_CONSISTENCY": true,
+    "LIBSBML_CAT_UNITS_CONSISTENCY": false,
+    "LIBSBML_CAT_MATHML_CONSISTENCY": true,
+    "LIBSBML_CAT_SBO_CONSISTENCY": true,
+    "LIBSBML_CAT_OVERDETERMINED_MODEL": true,
+    "LIBSBML_CAT_MODELING_PRACTICE": true,
+    "LIBSBML_CAT_STRICT_UNITS_CONSISTENCY": false,
+};
+
+
+utils().then((Module) => {
+
+    // instantiate validator
+    var instance = Module;
+
+
+    console.log(instance.getLibSBMLVersion());
+
+    // load a local file using the filesystem 
+    var data = fs.readFileSync(process.argv[2], 'utf8');
+
+    // print validation errors
+    console.log(instance.validateSBMLString(data, JSON.stringify(validationOptions)));
+
+});
