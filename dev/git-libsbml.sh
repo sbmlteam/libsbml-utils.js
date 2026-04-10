63f2c3cd8e863bfbe20b0d5c8c90bd52c17666e2
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2026-04-10 18:13:10 +0200

    - allow to override branch

--- dev/git-libsbml.sh
+++ dev/git-libsbml.sh
@@ -3,14 +3,15 @@ pushd "$(dirname "$0")"
 
 LIBSBML_REPO="https://github.com/sbmlteam/libsbml.git"
 LIBSBML_DIR="libsbml"
+LIBSBML_BRANCH="${LIBSBML_BRANCH:-development}"
 
 if [ ! -d "$LIBSBML_DIR" ]; then
     echo "Cloning libSBML repository..."
-    git clone "$LIBSBML_REPO" "$LIBSBML_DIR"
+    git clone -b $LIBSBML_BRANCH "$LIBSBML_REPO" "$LIBSBML_DIR"
 else
     echo "Updating libSBML repository..."
     pushd "$LIBSBML_DIR"
-    git pull origin development
+    git pull origin $LIBSBML_BRANCH
     popd
 fi
 
